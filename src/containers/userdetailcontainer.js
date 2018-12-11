2f58d2346482c9f32890bea7c7c7c535891485ae
Koshy <624865@cognizant.com>
2018-12-11 16:01:06 +0530

    first commit

--- src/containers/userdetailcontainer.js
+++ src/containers/userdetailcontainer.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { connect } from "react-redux";
 class UserDetailContainer extends React.Component {
     state = {  }
     render() { 
-        return (  <div>
-            User Details from container
-        </div>);
+            if(this.props.activeUser === null){
+                return(
+                    <div>Click to see the details</div>
+                )
+            }
+            else{
+                return(
+                    <div>
+                        User Details from container
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    Name:<b>{this.props.activeUser.first} {this.props.activeUser.last}</b><br></br>
+                    Age: {this.props.activeUser.age}<br></br>
+                    Comments: {this.props.activeUser.desc}<br></br>
+                    </div>
+                )
+            }
     }
 }
  
-export default UserDetailContainer;
+function convertStoreToProps(store){
+    return{
+        activeUser:store.clickeduser
+    }
+}
+
+export default connect(convertStoreToProps)(UserDetailContainer);
